f762e4680698a1131727d40c585117a64088dede
dima20-hud <57990756+dima20-hud@users.noreply.github.com>
2020-12-01 19:21:33 +0200

    Add files via upload

---
+++ js2.css.js
@@ -0,0 +1,22 @@
+function js2_f11()
+{
+document.getElementById("pic1").src="Вуди";
+document.getElementById("body1").style.backgroundColor="blue";
+document.getElementById("body1").style.backgroundColor="yellow";
+} 
+function js2_f12()
+{
+if(document.getElementById("sl1").selectedIndex==0)
+document.getElementById("body1").style.backgroundColor="green";
+ 
+if(document.getElementById("sl1").selectedIndex==1)
+document.getElementById("body1").style.backgroundColor="red";
+ 
+if(document.getElementById("sl1").selectedIndex==2)
+document.getElementById("body1").style.backgroundColor="pink";
+}
+function js2_f13()
+{
+document.getElementById("area1").value="Привет, Мир!!!";
+ 
+}
