7e582f6d34f4c69b7a4b56443171ef23a48ac779
dima20-hud <57990756+dima20-hud@users.noreply.github.com>
2020-12-02 08:50:14 +0200

    Update js2.css.js

--- js2.css.js
+++ js2.css.js
@@ -2,7 +2,7 @@ function js2_f11()
 {
 document.getElementById("pic1").src="Вуди.ipj";
 document.getElementById("body1").style.backgroundColor="blue";
-document.getElementById("body2").style.backgroundColor="yellow";
+document.getElementById("body1").style.backgroundColor="yellow";
 } 
 function js2_f12()
 {
@@ -10,10 +10,10 @@ if(document.getElementById("sl1").selectedIndex=0)
 document.getElementById("body1").style.backgroundColor="green";
  
 if(document.getElementById("sl2").selectedIndex=1)
-document.getElementById("body2").style.backgroundColor="red";
+document.getElementById("body1").style.backgroundColor="red";
  
 if(document.getElementById("sl3").selectedIndex=2)
-document.getElementById("body3").style.backgroundColor="pink";
+document.getElementById("body1").style.backgroundColor="pink";
 }
 function js2_f13()
 {
