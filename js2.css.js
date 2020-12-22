35edd394ced21b65870c679c61074b4fee815eea
dima20-hud <57990756+dima20-hud@users.noreply.github.com>
2020-12-23 00:23:35 +0200

    Update js2.css.js

--- js2.css.js
+++ js2.css.js
@@ -1,4 +1,9 @@
- function g(){if(document.getElementById("sl1").selectedIndex==0)
+function f(){
+document.getElementById("pic1").src="2.gif";
+document.getElementById("body1").style.backgroundColor="blue";
+document.getElementById("body1").style.backgroundColor="yellow";
+}
+function g(){if(document.getElementById("sl1").selectedIndex==0)
 document.getElementById("body1").style.backgroundColor="green";
 if(document.getElementById("sl1").selectedIndex==1)document.getElementById("body1").style.backgroundColor="red";
 if(document.getElementById("sl1").selectedIndex==2)
